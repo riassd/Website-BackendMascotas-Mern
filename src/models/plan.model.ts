f79d89e200bf5a599ff52fa479c862aa573201ee
riassd <95007181+riassd@users.noreply.github.com>
2022-11-26 10:35:21 -0500

    Update plan.model.ts

--- src/models/plan.model.ts
+++ src/models/plan.model.ts
@@ -23,10 +23,10 @@ export class Plan extends Entity {
   descripcion: string;
 
   @property({
-    type: 'number',
+    type: 'string',
     required: true,
   })
-  precio: number;
+  precio: string;
 
   @hasMany(() => Mascota)
   mascotas: Mascota[];
