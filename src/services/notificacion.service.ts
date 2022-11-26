379d91b65eea5b72cfe5de090d1cbf34cf41ed53
riassd <95007181+riassd@users.noreply.github.com>
2022-11-26 13:11:35 -0500

    Add files via upload

--- src/services/notificacion.service.ts
+++ src/services/notificacion.service.ts
@@ -1,8 +1,31 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-
+import {repository} from '@loopback/repository';
+import {UsuarioRepository} from '../repositories';
+const sgMail = require('@sendgrid/mail');
 @injectable({scope: BindingScope.TRANSIENT})
 export class NotificacionService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(
+    /* Add @inject to inject parameters */
+    @repository(UsuarioRepository)
+    public usuarioRepository: UsuarioRepository,
+  ) { }
+  enviarCorreo(destino: string, asunto: string, cuerpo: string) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: destino,
+      from: process.env.CORREO_SENDGRID,
+      subject: asunto,
+      html: cuerpo,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Correo electrónico enviado');
+      })
+      .catch((error: unknown) => {
+        console.error(error);
+      });
+  }
 
   /*
    * Add service methods here
